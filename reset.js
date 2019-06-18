ac02aa6466d1a2ed30c8e9bd4f5ebf04509e20d4
restze <50900028+restze@users.noreply.github.com>
2019-06-18 02:30:08 +0100

    Update reset.js

--- reset.js
+++ reset.js
@@ -35,7 +35,111 @@ bthtml += "<li><a href=\"#\" onclick='play(\"99\"); getEpgCH(99);' class=\"btn-p
 bthtml += "<li><a href=\"#\" onclick='play(\"100\"); getEpgCH(100);' class=\"btn-primary m-1 24\"><span class=\"bespain\">Jeem</span><span class=\"besubspan\">beIN</span></a></li>";
 document.getElementById("beinnavAR").innerHTML = bthtml;
 var spiner = "spiner.png";
-var bespainz = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9zc28iOiI1YmExMjA1ZGU0NGM4Y2IwYjk3ZmQ0ZThARnVibyIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfbmFtZSI6IiIsInNjb3BlIjpbXSwiaWRfaWRlbnRpdHkiOjg1NzI4LCJzc29fcHJvdmlkZXIiOiJhZG9iZXBhc3MiLCJhcGlwIjoiNTEuMTUuMTQxLjE3OSIsImV4cCI6MTU2MDkyODU2NTkyMywiYXV0aG9yaXRpZXMiOlsiRW5kVXNlclJvbGUiXSwianRpIjoiNGI5OGI0NWItMTg0ZC00ODM3LWE5NjItMTE1ZTk3ZmQ4OTkyIiwiY2xpZW50X2lkIjoidHVjYW5vVHJ1c3RlZFVzZXIifQ.vzJSOurFDV4bP9uI9pC_2dWi1Ue7iDS-4ktvS03NImk2fY0AYrQQUzVeOCqHhimUPlofL6fOFz8BzYblueuEA3AFugPx-RSNwRFgonCglZ4eq105noClo21dA7De9n9K0vqeNuwGL3ogCkfgO8N4LGzq0p-ZDJtLXfKxri5i3Epmui-Ym4uQcnjCaN2WXstH-Is15pYTRwKgDGvkBX91Ao9gcw4dWaLJBUgYWOb7Xl9C71zx_s-mWvHjYR6xgiV3F47eRNaB8HEA7sbKVA3AgV5ttD4RDwHuumZ8Xb3MvaVtX0Ew8W8P_L3omSRcUSGN-BkmWbw5Et0Fw7_CbwGhFA";
-var bespai = "12";
+var SHA256 = {
+
+
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    decode: function(input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+
+        }
+
+        output = SHA256._utf8_decode(output);
+
+        return output;
+
+    },
+
+    _utf8_encode: function(string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+
+        return utftext;
+    },
+
+    _utf8_decode: function(utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while (i < utftext.length) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        }
+
+        return string;
+    }
+
+}
+    
+    
+    
+
+var $strj = "ZXlKaGJHY2lPaUpTVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBaRjl6YzI4aU9pSTFZbUV4TWpBMVpHVTBOR000WTJJd1lqazNabVEwWlRoQVJuVmlieUlzSW1keVlXNTBYM1I1Y0dVaU9pSndZWE56ZDI5eVpDSXNJblZ6WlhKZmJtRnRaU0k2SWlJc0luTmpiM0JsSWpwYlhTd2lhV1JmYVdSbGJuUnBkSGtpT2pnMU56STRMQ0p6YzI5ZmNISnZkbWxrWlhJaU9pSmhaRzlpWlhCaGMzTWlMQ0poY0dsd0lqb2lOVEV1TVRVdU1UUXhMakUzT1NJc0ltVjRjQ0k2TVRVMk1Ea3lPRFUyTlRreU15d2lZWFYwYUc5eWFYUnBaWE1pT2xzaVJXNWtWWE5sY2xKdmJHVWlYU3dpYW5ScElqb2lOR0k1T0dJME5XSXRNVGcwWkMwME9ETTNMV0U1TmpJdE1URTFaVGszWm1RNE9Ua3lJaXdpWTJ4cFpXNTBYMmxrSWpvaWRIVmpZVzV2VkhKMWMzUmxaRlZ6WlhJaWZRLnZ6SlNPdXJGRFY0YlA5dUk5cENfMmRXaTFVZTdpRFMtNGt0dlMwM05JbWsyZlkwQVlyUVFVelZlT0NxSGhpbVVQbG9mTDZmT0Z6OEJ6WWJsdWV1RUEzQUZ1Z1B4LVJTTndSRmdvbkNnbFo0ZXExMDVub0NsbzIxZEE3RGU5bjlLMHZxZU51d0dMM29nQ2tmZ084TjRMR3pxMHAtWkRKdExYZkt4cmk1aTNFcG11aS1ZbTR1UWNuakNhTjJXWHN0SC1JczE1cFlUUndLZ0RHdmtCWDkxQW85Z2N3NGRXYUxKQlVnWVdPYjdYbDlDNzF6eF9zLW1XdkhqWVI2eGdpVjNGNDdlUk5hQjhIRUE3c2JLVkEzQWdWNXR0RDRSRHdIdXVtWjhYYjNNdmFWdFgwRXc4VzhQX0wzb21TUmNVU0dOLUJrbVdidzVFdDBGdzdfQ2J3R2hGQQ==";
+var $strt = "MTI=";
+var bespainz = SHA256.decode($strj);
+var bespai = SHA256.decode($strt);
 var bespain = "<?xml version='1.0' encoding='utf-8'?><LicenseRequest xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' Version='1.1'><LicenseType>Persistent</LicenseType><FirstPlayExpiration>P1D</FirstPlayExpiration><BeginUtcDateTime>2016-08-31T14:13:19Z</BeginUtcDateTime><ExpirationUtcDateTime>2036-09-24T21:59:59Z</ExpirationUtcDateTime><DigitalVideoProtectionLevel>0</DigitalVideoProtectionLevel><AnalogVideoProtectionLevel>0</AnalogVideoProtectionLevel><AuthToken>"+bespainz+"</AuthToken><idChannel>"+bespai+"</idChannel></LicenseRequest>";
 //var bespain = "<?xml version='1.0' encoding='utf-8'?><LicenseRequest xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' Version='1.1'><LicenseType>Persistent</LicenseType><FirstPlayExpiration>P1D</FirstPlayExpiration><BeginUtcDateTime>2016-08-31T14:13:19Z</BeginUtcDateTime><ExpirationUtcDateTime>2036-09-24T21:59:59Z</ExpirationUtcDateTime><DigitalVideoProtectionLevel>0</DigitalVideoProtectionLevel><AnalogVideoProtectionLevel>0</AnalogVideoProtectionLevel><AuthToken>eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9zc28iOiI1YmExMjA1ZGU0NGM4Y2IwYjk3ZmQ0ZThARnVibyIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfbmFtZSI6IiIsInNjb3BlIjpbXSwiaWRfaWRlbnRpdHkiOjg1NzI4LCJzc29fcHJvdmlkZXIiOiJhZG9iZXBhc3MiLCJhcGlwIjoiNTEuMTUuMTQxLjE3OSIsImV4cCI6MTU2MDkyODU2NTkyMywiYXV0aG9yaXRpZXMiOlsiRW5kVXNlclJvbGUiXSwianRpIjoiNGI5OGI0NWItMTg0ZC00ODM3LWE5NjItMTE1ZTk3ZmQ4OTkyIiwiY2xpZW50X2lkIjoidHVjYW5vVHJ1c3RlZFVzZXIifQ.vzJSOurFDV4bP9uI9pC_2dWi1Ue7iDS-4ktvS03NImk2fY0AYrQQUzVeOCqHhimUPlofL6fOFz8BzYblueuEA3AFugPx-RSNwRFgonCglZ4eq105noClo21dA7De9n9K0vqeNuwGL3ogCkfgO8N4LGzq0p-ZDJtLXfKxri5i3Epmui-Ym4uQcnjCaN2WXstH-Is15pYTRwKgDGvkBX91Ao9gcw4dWaLJBUgYWOb7Xl9C71zx_s-mWvHjYR6xgiV3F47eRNaB8HEA7sbKVA3AgV5ttD4RDwHuumZ8Xb3MvaVtX0Ew8W8P_L3omSRcUSGN-BkmWbw5Et0Fw7_CbwGhFA</AuthToken><idChannel>12</idChannel></LicenseRequest>";
